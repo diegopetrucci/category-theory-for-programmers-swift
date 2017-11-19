eb8ee05a3c90e094b458074d4ca629aae6b090f1
Diego Petrucci <diego.petrucci@babylonhealth.com>
2017-11-19 22:22:47 +0000

    Add `memoize` function

--- Category Theory.playground/Pages/Chapter 2.xcplaygroundpage/Contents.swift
+++ Category Theory.playground/Pages/Chapter 2.xcplaygroundpage/Contents.swift
@@ -1,28 +1,50 @@
 //: [Previous](@previous)
 
+/*
+ Define a higher-order function (or a function object) memoize in your favorite language.
+ This function takes a pure function f as an argument and returns a function that behaves almost exactly like f, except that it only calls the original function once for every argument, stores the result internally, and subsequently returns this stored result every time it’s called with the same argument.
+ You can tell the memoized function from the original by watching its performance. For instance, try to memoize a function that takes a long time to evaluate.
+ You’ll have to wait for the result the first time you call it, but on subsequent calls, with the same argument, you should get the result immediately.
+ */
 
+func memoize<T: Hashable, U>(f: @escaping (T) -> U) -> (T) -> U {
+    var cache: [T: U] = [:]
 
+    return { x in
+        if let u = cache[x] {
+            return u
+        } else {
+            let u = f(x)
+            cache[x] = u
+            return u
+        }
+    }
+}
 
+func fibonacci(x: Int) -> Int {
+    if x == 0 || x == 1 { return x }
+    else {
+        return fibonacci(x: x - 1) + fibonacci(x: x - 2)
+    }
+}
 
+let memoizedFibonacci = memoize(f: fibonacci)
 
+let memoizedFibonacci2 = memoize { (x: Int) -> Int in
+    if x == 0 || x == 1 { return x }
+    else {
+        return fibonacci(x: x - 1) + fibonacci(x: x - 2)
+    }
+}
 
+memoizedFibonacci(20)
+memoizedFibonacci(20)
+memoizedFibonacci(20)
+memoizedFibonacci(20)
 
-
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+memoizedFibonacci2(20)
+memoizedFibonacci2(20)
+memoizedFibonacci2(20)
+memoizedFibonacci2(20)
 
 //: [Next](@next)
