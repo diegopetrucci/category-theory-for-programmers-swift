3328fadd204f89204ef7d177f3eb5bee8e143184
Diego Petrucci <diego.petrucci@babylonhealth.com>
2017-11-19 16:50:09 +0000

    Add first chapter exercise + empty chapter 2 page

---
+++ Category Theory.playground/Pages/Chapter 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,52 @@
+/*
+ Implement, as best as you can, the identity function in your fa- vorite language
+ (or the second favorite, if your favorite language happens to be Haskell).
+*/
+
+func identity<T, U>(f: @escaping (T) -> U) -> (T) -> U {
+    return f
+}
+
+func double(x: Int) -> Int { return x * 2 }
+
+let doubleIdentity = identity(f: double)
+
+double(x: 5)
+doubleIdentity(5)
+
+/*
+ Implement the composition function in your favorite language.
+ It takes two functions as arguments and returns a function that is their composition.
+*/
+
+func composition<T, U, V>(f: @escaping (T) -> U, g: @escaping (U) -> V) -> (T) -> V {
+    return { x in
+        return g(f(x))
+    }
+}
+
+func plusOne(x: Int) -> Int { return x + 1 }
+
+let doubleAndThenPlusOne = composition(f: double, g: plusOne)
+
+doubleAndThenPlusOne(5)
+
+/*
+ Write a program that tries to test that your composition function respects identity.
+*/
+
+// this does not compile
+
+let plusOneIdentity = identity(f: plusOne)
+
+func identityTest<T, U, V>(compositionFunction: ((T) -> U, (U) -> V) -> (T) -> V)
+    -> Bool {
+        compositionFunction(doubleIdentity, plusOneIdentity)
+    let doubleAndThenPlusOneIdentity = compositionFunction(doubleIdentity, plusOneIdentity)
+    let doubleAndThenPlusOneResult = doubleAndThenPlusOne(5)
+    let doubleAndThenPlusOneIdentityResylt = doubleAndThenPlusOneIdentity(5)
+    return doubleAndThenPlusOneResult == doubleAndThenPlusOneIdentityResylt
+}
+
+let doesCompositionRespectIdentity = identityTest(composition: composition)
+
